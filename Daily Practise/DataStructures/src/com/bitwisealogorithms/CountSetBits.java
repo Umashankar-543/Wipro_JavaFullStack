67eb9c97f96b4ef38bbb08cfb44c6b2be4075652
M.Umashankar Reddy <umashankarmedapati543@gmail.com>
2024-05-31 11:04:00 +0530

    Delete Daily Practise/DataStructures directory

--- Daily Practise/DataStructures/src/com/bitwisealogorithms/CountSetBits.java
+++
@@ -1,8 +0,0 @@
-package com.bitwisealogorithms;
-
-public class CountSetBits {
-	public static void main(String[] args) {
-		int num = 55;
-		System.out.println("Number of set bits in " + num + " is " + Integer.bitCount(num));
-	}
-}
